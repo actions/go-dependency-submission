1e94bc99021b90810e0df3f85018020b8b24c23c
Lane Seppala <lseppala@users.noreply.github.com>
2022-05-14 01:05:41 +0000

    Lint and remove go.sum search

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,57 @@
-import * as core from '@actions/core'
-import { run } from '@github/dependency-submission-toolkit'
-import { ProcessDependenciesContent } from '@github/dependency-submission-toolkit/dist/processor'
-import { parseDependents } from './go_mod_parser'
-import * as path from 'path'
-import * as process from 'process'
-import execa from 'execa'
+import * as core from "@actions/core";
+import { run } from "@github/dependency-submission-toolkit";
+import { ProcessDependenciesContent } from "@github/dependency-submission-toolkit/dist/processor";
+import { parseDependents } from "./go_mod_parser";
+import * as path from "path";
+import * as process from "process";
+import execa from "execa";
 
-const parseDependentsFunc: ProcessDependenciesContent = parseDependents
+const parseDependentsFunc: ProcessDependenciesContent = parseDependents;
 
 // Set the detector information provided from the action workflow input
 const detector = {
-  name: core.getInput('detector-name'),
-  url: core.getInput('detector-url'),
-  version: core.getInput('detector-version')
-}
+  name: core.getInput("detector-name"),
+  url: core.getInput("detector-url"),
+  version: core.getInput("detector-version"),
+};
 
-async function searchForFile (filename:string) {
-  console.log(`searching for ${filename} in ${process.cwd()}`)
+async function searchForFile(filename: string) {
+  console.log(`searching for ${filename} in ${process.cwd()}`);
 
-  const { stdout } = await execa('find', [
-    process.cwd(),
-    '-name',
-    filename
-  ])
+  const { stdout } = await execa("find", [process.cwd(), "-name", filename]);
 
   const dirs = stdout
-    .split('\n')
-    .filter(s => s.length > 0)
+    .split("\n")
+    .filter((s) => s.length > 0)
     // remove the file name
     .map((filename) => path.dirname(filename))
     // map to absolute path
-    .map((pathname) => path.resolve(process.cwd(), pathname))
+    .map((pathname) => path.resolve(process.cwd(), pathname));
 
-  return dirs
+  return dirs;
 }
 
 // Enumerate directories
-async function detect () {
-  const goModPaths = searchForFile('go.mod')
-  const goSumPaths = searchForFile('go.sum')
-
-  // Concatenate both lists and remove duplicates
-  const allPaths = new Set((await goModPaths).concat(await goSumPaths))
+async function detect() {
+  const goModPaths = await searchForFile("go.mod");
 
   // If provided, set the metadata provided from the action workflow input
-  const metadataInput = core.getInput('metadata')
+  const metadataInput = core.getInput("metadata");
 
-  allPaths.forEach((path) => {
-    process.chdir(path)
-    console.log(`Running go mod graph in ${path}`)
+  goModPaths.forEach((path) => {
+    process.chdir(path);
+    console.log(`Running go mod graph in ${path}`);
     if (metadataInput.length < 1) {
-      run(parseDependentsFunc, { command: 'go mod graph' }, { detector })
+      run(parseDependentsFunc, { command: "go mod graph" }, { detector });
     } else {
-      const metadata = JSON.parse(metadataInput)
-      run(parseDependentsFunc, { command: 'go mod graph' }, { metadata, detector })
+      const metadata = JSON.parse(metadataInput);
+      run(
+        parseDependentsFunc,
+        { command: "go mod graph" },
+        { metadata, detector }
+      );
     }
-  })
+  });
 }
 
-detect()
+detect();
--- test/go_mod_parser.test.ts
+++ test/go_mod_parser.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, test } from '@jest/globals'
-import { parseDependents } from '../src/go_mod_parser'
+import { describe, expect, test } from "@jest/globals";
+import { parseDependents } from "../src/go_mod_parser";
 
 /*
 Expected dependency tree:
@@ -22,26 +22,26 @@ github.com/jaffee/commandeer@v0.5.0 github.com/spf13/pflag@v1.0.3
 github.com/jaffee/commandeer@v0.5.0 github.com/spf13/viper@v1.4.0
 github.com/spf13/viper@v1.4.0 github.com/armon/consul-api@v0.0.0-20180202201655-eb2c6b5be1b6
 github.com/spf13/viper@v1.4.0 github.com/coreos/bbolt@v1.3.2
-`
+`;
 
-describe('test go mod graph dependenciesProcessorFunc', () => {
-  test('parses output of go mod graph into dependencies', async () => {
-    const dependencies = parseDependents(GO_MOD_GRAPH)
+describe("test go mod graph dependenciesProcessorFunc", () => {
+  test("parses output of go mod graph into dependencies", async () => {
+    const dependencies = parseDependents(GO_MOD_GRAPH);
 
     // Should have 7 total dependencies: 2 direct, 5 indirect
-    expect(Object.values(dependencies).length).toEqual(7)
+    expect(Object.values(dependencies).length).toEqual(7);
 
     expect(
-      dependencies['pkg:golang/github.com/jaffee/commandeer@v0.5.0']
+      dependencies["pkg:golang/github.com/jaffee/commandeer@v0.5.0"]
         .relationship
-    ).toEqual('direct')
+    ).toEqual("direct");
 
     expect(
-      dependencies['pkg:golang/github.com/spf13/cobra@v0.0.3'].relationship
-    ).toEqual('direct')
+      dependencies["pkg:golang/github.com/spf13/cobra@v0.0.3"].relationship
+    ).toEqual("direct");
 
     expect(
-      dependencies['pkg:golang/github.com/spf13/viper@v1.4.0'].dependencies
-    ).toContainEqual(dependencies['pkg:golang/github.com/coreos/bbolt@v1.3.2'])
-  })
-})
+      dependencies["pkg:golang/github.com/spf13/viper@v1.4.0"].dependencies
+    ).toContainEqual(dependencies["pkg:golang/github.com/coreos/bbolt@v1.3.2"]);
+  });
+});
