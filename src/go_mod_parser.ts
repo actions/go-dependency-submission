c66a139385291e5fef7ecc7efb404f95d59797ab
Lane Seppala <lseppala@users.noreply.github.com>
2022-05-14 01:11:55 +0000

    More lint

--- src/go_mod_parser.ts
+++ src/go_mod_parser.ts
@@ -1,45 +1,45 @@
 import {
   Entry,
-  ParsedDependencies,
-} from "@github/dependency-submission-toolkit/dist/processor";
+  ParsedDependencies
+} from '@github/dependency-submission-toolkit/dist/processor'
 
-export function parseDependents(contents: string) {
-  const stdoutArr: string[] = contents.split("\n");
+export function parseDependents (contents: string) {
+  const stdoutArr: string[] = contents.split('\n')
   const splitStdoutArr: string[][] = stdoutArr.map(function (line) {
-    return line.split(" ");
-  });
+    return line.split(' ')
+  })
 
-  const entries: ParsedDependencies = {};
-  const repoName = splitStdoutArr[0][0];
+  const entries: ParsedDependencies = {}
+  const repoName = splitStdoutArr[0][0]
   splitStdoutArr.forEach((line: string[]) => {
-    if (line === undefined || line.length < 2) return; // skip empty lines
+    if (line === undefined || line.length < 2) return // skip empty lines
 
-    let targetEntry: Entry;
-    const targetPkg = `pkg:golang/${line[0]}`;
-    let dependencyEntry: Entry;
-    const dependencyPkg = `pkg:golang/${line[1]}`;
+    let targetEntry: Entry
+    const targetPkg = `pkg:golang/${line[0]}`
+    let dependencyEntry: Entry
+    const dependencyPkg = `pkg:golang/${line[1]}`
 
-    const matchFound = line[0].match(repoName);
+    const matchFound = line[0].match(repoName)
     if (matchFound && matchFound.index != null) {
-      entries[dependencyPkg] = new Entry(dependencyPkg, "direct");
-      return;
+      entries[dependencyPkg] = new Entry(dependencyPkg, 'direct')
+      return
     }
 
     if (targetPkg in entries) {
-      targetEntry = entries[targetPkg];
+      targetEntry = entries[targetPkg]
     } else {
-      targetEntry = new Entry(targetPkg, "indirect");
-      entries[targetPkg] = targetEntry;
+      targetEntry = new Entry(targetPkg, 'indirect')
+      entries[targetPkg] = targetEntry
     }
 
     if (dependencyPkg in entries) {
-      dependencyEntry = entries[dependencyPkg];
+      dependencyEntry = entries[dependencyPkg]
     } else {
-      dependencyEntry = new Entry(dependencyPkg, "indirect");
-      entries[dependencyPkg] = dependencyEntry;
+      dependencyEntry = new Entry(dependencyPkg, 'indirect')
+      entries[dependencyPkg] = dependencyEntry
     }
 
-    targetEntry.addDependency(dependencyEntry);
-  });
-  return entries;
+    targetEntry.addDependency(dependencyEntry)
+  })
+  return entries
 }
--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,57 @@
-import * as core from "@actions/core";
-import { run } from "@github/dependency-submission-toolkit";
-import { ProcessDependenciesContent } from "@github/dependency-submission-toolkit/dist/processor";
-import { parseDependents } from "./go_mod_parser";
-import * as path from "path";
-import * as process from "process";
-import execa from "execa";
+import * as core from '@actions/core'
+import { run } from '@github/dependency-submission-toolkit'
+import { ProcessDependenciesContent } from '@github/dependency-submission-toolkit/dist/processor'
+import { parseDependents } from './go_mod_parser'
+import * as path from 'path'
+import * as process from 'process'
+import execa from 'execa'
 
-const parseDependentsFunc: ProcessDependenciesContent = parseDependents;
+const parseDependentsFunc: ProcessDependenciesContent = parseDependents
 
 // Set the detector information provided from the action workflow input
 const detector = {
-  name: core.getInput("detector-name"),
-  url: core.getInput("detector-url"),
-  version: core.getInput("detector-version"),
-};
+  name: core.getInput('detector-name'),
+  url: core.getInput('detector-url'),
+  version: core.getInput('detector-version')
+}
 
-async function searchForFile(filename: string) {
-  console.log(`searching for ${filename} in ${process.cwd()}`);
+async function searchForFile (filename: string) {
+  console.log(`searching for ${filename} in ${process.cwd()}`)
 
-  const { stdout } = await execa("find", [process.cwd(), "-name", filename]);
+  const { stdout } = await execa('find', [process.cwd(), '-name', filename])
 
   const dirs = stdout
-    .split("\n")
+    .split('\n')
     .filter((s) => s.length > 0)
     // remove the file name
     .map((filename) => path.dirname(filename))
     // map to absolute path
-    .map((pathname) => path.resolve(process.cwd(), pathname));
+    .map((pathname) => path.resolve(process.cwd(), pathname))
 
-  return dirs;
+  return dirs
 }
 
 // Enumerate directories
-async function detect() {
-  const goModPaths = await searchForFile("go.mod");
+async function detect () {
+  const goModPaths = await searchForFile('go.mod')
 
   // If provided, set the metadata provided from the action workflow input
-  const metadataInput = core.getInput("metadata");
+  const metadataInput = core.getInput('metadata')
 
   goModPaths.forEach((path) => {
-    process.chdir(path);
-    console.log(`Running go mod graph in ${path}`);
+    process.chdir(path)
+    console.log(`Running go mod graph in ${path}`)
     if (metadataInput.length < 1) {
-      run(parseDependentsFunc, { command: "go mod graph" }, { detector });
+      run(parseDependentsFunc, { command: 'go mod graph' }, { detector })
     } else {
-      const metadata = JSON.parse(metadataInput);
+      const metadata = JSON.parse(metadataInput)
       run(
         parseDependentsFunc,
-        { command: "go mod graph" },
+        { command: 'go mod graph' },
         { metadata, detector }
-      );
+      )
     }
-  });
+  })
 }
 
-detect();
+detect()
