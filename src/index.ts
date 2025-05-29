a493d679211785b9244276e7e6e0c9d468eceedb
E. Lynette Rayle <elrayle@users.noreply.github.com>
2025-05-29 15:27:47 +0000

    revert to directly using input for sha and ref and allowing the context to drive them if they are missing

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import {
   Manifest,
   submitSnapshot
 } from '@github/dependency-submission-toolkit'
-import type { PullRequestEvent } from '@octokit/webhooks-types'
 
 import {
   processGoGraph,
@@ -96,11 +95,7 @@ async function main () {
       url: 'https://github.com/actions/go-dependency-submission',
       version: '0.0.1'
     }
-  } else if (
-    detectorName === '' ||
-    detectorUrl === '' ||
-    detectorVersion === ''
-  ) {
+  } else if (detectorName === '' || detectorUrl === '' || detectorVersion === '') {
     // if any of detectorName, detectorUrl, or detectorVersion have value, then they are all required
     throw new Error(
       "Invalid input: if any of 'detector-name', 'detector-url', or 'detector-version' have value, then thay are all required."
@@ -119,29 +114,10 @@ async function main () {
     id: github.context.runId.toString()
   })
   snapshot.addManifest(manifest)
-  snapshot.sha = core.getInput('snapshot-sha') && getShaFromContext()
-  snapshot.ref = core.getInput('snapshot-ref') && github.context.ref
+  snapshot.sha = core.getInput('snapshot-sha')
+  snapshot.ref = core.getInput('snapshot-ref')
 
   submitSnapshot(snapshot)
-}
-
-function getShaFromContext (): string {
-  const context = github.context
-  const pullRequestEvents = [
-    'pull_request',
-    'pull_request_comment',
-    'pull_request_review',
-    'pull_request_review_comment'
-    // Note that pull_request_target is omitted here.
-    // That event runs in the context of the base commit of the PR,
-    // so the snapshot should not be associated with the head commit.
-  ]
-  if (pullRequestEvents.includes(context.eventName)) {
-    const pr = (context.payload as PullRequestEvent).pull_request
-    return pr.head.sha
-  } else {
-    return context.sha
-  }
 }
 
 main()
